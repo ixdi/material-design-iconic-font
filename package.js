be43dd2fc785e009702e42fd013a91ba74fcdbed
ixdi <digistilts@gmail.com>
2014-11-27 17:03:06 +0100

    Added files

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+    name: 'ixdi:material-design-iconic-font',
+    summary: 'Material Design iconic font and CSS toolkit by zavoloklom',
+    version: '1.0.1',
+    git: 'https://github.com/ixdi/material-design-iconic-font.git'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+    api.addFiles([
+        'material-design-iconic-font/fonts/Material-Design-Iconic-Font.woff',
+        'material-design-iconic-font/fonts/Material-Design-Iconic-Font.ttf',
+        'material-design-iconic-font/fonts/Material-Design-Iconic-Font.svg',
+        'material-design-iconic-font/css/material-design-iconic-font.css',
+        'fonts-override.css'
+    ], 'client');
+});
