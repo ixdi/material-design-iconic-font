f5545e8265f10f04034ae343feee4fbbbd9cc2a8
ixdi <digistilts@gmail.com>
2015-09-24 11:10:25 +0200

    Fixed typo

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: 'ixdidos:material-design-iconic-font',
+    name: 'ixdi:material-design-iconic-font',
     summary: 'Material Design Icons Font v2.0',
     version: '2.1.4',
     git: 'https://github.com/ixdi/material-design-iconic-font.git'
